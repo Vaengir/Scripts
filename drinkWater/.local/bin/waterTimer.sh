8cd93dec8c5658e7124ca9de833ae531521eddc5
Vaengir <vaengir@gmx.de>
2023-03-21 09:37:40 +0100

    Changed the way echo works
    Now only echos the time between notifications once at the start.

--- drinkWater/.local/bin/waterTimer.sh
+++ drinkWater/.local/bin/waterTimer.sh
@@ -1,10 +1,13 @@
+#! /usr/bin/bash
+
+if [[ $1 ]]; then
+  echo "The Timer was set to $(($1/60)) min"
+  sleepTime=$1;
+else
+  echo "No Timer set, using default 15 min";
+  sleepTime=9000;
+fi
 while true; do
   notify-send -i ~/.local/bin/waterIcon.png "Time to drink Water!!";
-  if [[ $1 ]]; then
-    echo "The Timer was set to $(($1/60)) min"
-    sleep $1;
-  else
-    echo "No Timer set, using default 15 min";
-    sleep 9000;
-  fi
+  sleep $sleepTime
 done
