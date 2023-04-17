d0bf85577a484ae7ea59ed054264d76a6c3d1347
Vaengir <vaengir@gmx.de>
2023-04-17 09:19:34 +0200

    now supresses usual error output

--- xrandrDesktop/.local/bin/monitorSetup.sh
+++ xrandrDesktop/.local/bin/monitorSetup.sh
@@ -4,4 +4,4 @@ xrandr --output DisplayPort-1 --primary --auto
 echo "First Screen configured"
 xrandr --output DisplayPort-2 --right-of DisplayPort-1 --rotate right --auto
 echo "Second Screen configured"
-echo 'awesome.restart()' | awesome-client
+echo 'awesome.restart()' | awesome-client &> /dev/null
