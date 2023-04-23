bafaf951512db4d2ca20dd3bd1531540f3247aa6
Vaengir <vaengir@gmx.de>
2023-04-23 12:44:22 +0200

    fix: Now works as intended again
    I don't know what broke it but this version of the script now works
    again.

--- xrandrDesktop/.local/bin/monitorSetup.sh
+++ xrandrDesktop/.local/bin/monitorSetup.sh
@@ -1,7 +1,12 @@
 #! /usr/bin/bash
 
+awesome_restart(){
+  /bin/su $USER -c "echo 'awesome.restart()' | awesome-client"
+}
+
 xrandr --output DisplayPort-1 --primary --auto
 echo "First Screen configured"
 xrandr --output DisplayPort-2 --right-of DisplayPort-1 --rotate right --auto
 echo "Second Screen configured"
-echo 'awesome.restart()' | awesome-client &> /dev/null
+
+awesome_restart
