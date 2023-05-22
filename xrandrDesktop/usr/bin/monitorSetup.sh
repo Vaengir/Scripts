cd65bf8a07e3fe922049587ca657d76c7cd4c242
Vaengir <vaengir@gmx.de>
2023-05-22 09:44:47 +0200

    feat: Now only runs with sudo
    Also displays proper error message.

--- xrandrDesktop/usr/bin/monitorSetup.sh
+++ xrandrDesktop/usr/bin/monitorSetup.sh
@@ -1,5 +1,13 @@
 #! /usr/bin/bash
 
+RED='\033[0;31m' 
+NOCOLOR='\033[0m'
+
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+  printf "${RED}Error:${NOCOLOR} Script needs root to run" 1>&2
+  exit
+fi
+
 awesome_restart(){
   /bin/su $USER -c "echo 'awesome.restart()' | awesome-client"
 }
