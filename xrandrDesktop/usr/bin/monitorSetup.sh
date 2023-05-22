67be5ceffaa884c7d4c13db2da69c7e165dcf0a3
Vaengir <vaengir@gmx.de>
2023-05-22 12:16:00 +0200

    fix: Script doesn't work with sudo

--- xrandrDesktop/usr/bin/monitorSetup.sh
+++ xrandrDesktop/usr/bin/monitorSetup.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/bash
 
-RED='\033[0;31m' 
-NOCOLOR='\033[0m'
+# RED='\033[0;31m' 
+# NOCOLOR='\033[0m'
 
-if [[ $(/usr/bin/id -u) -ne 0 ]]; then
-  printf "${RED}Error:${NOCOLOR} Script needs root to run" 1>&2
-  echo ""
-  exit
-fi
+# if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+#   printf "${RED}Error:${NOCOLOR} Script needs root to run" 1>&2
+#   echo ""
+#   exit
+# fi
 
 awesome_restart(){
   /bin/su $USER -c "echo 'awesome.restart()' | awesome-client"
