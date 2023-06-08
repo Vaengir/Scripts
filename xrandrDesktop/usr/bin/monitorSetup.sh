8fbbc5648b19567c8f7d99b6f1055a73d4b984f6
Vaengir <vaengir@gmx.de>
2023-06-08 13:06:35 +0200

    fix: Monitor now uses proper refresh rate

--- xrandrDesktop/usr/bin/monitorSetup.sh
+++ xrandrDesktop/usr/bin/monitorSetup.sh
@@ -13,7 +13,7 @@ awesome_restart(){
   /bin/su $USER -c "echo 'awesome.restart()' | awesome-client"
 }
 
-xrandr --output DisplayPort-1 --primary --auto
+xrandr --output DisplayPort-1 --primary --mode 2560x1440 --rate 164.83
 echo "First Screen configured"
 xrandr --output DisplayPort-2 --right-of DisplayPort-1 --rotate right --auto
 echo "Second Screen configured"
