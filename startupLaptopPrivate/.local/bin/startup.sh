432525d618e51ac3efd9bc707724c751bdde429f
Vaengir <vaengir@gmx.de>
2023-06-17 12:31:38 +0200

    fix: Made differenciation between work und private laptop

---
+++ startupLaptopPrivate/.local/bin/startup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Sets background of root window to black
+xsetroot -solid black 
+# Sets Cursor
+xsetroot -cursor_name top_left_arrow 
+# Bell Volume 0
+xset b 0 
+# Screensaver and Screen blanking off
+xset s off 
+xset -dpms 
+xset s noblank 
+# Run Compositor
+picom &
+# Allow Mousepad tapping to click
+xinput set-prop "$(xinput list --name-only | grep -i touch)" "libinput Tapping Enabled" 1
+# Update all repos
+g pull ~/personal/git/
