cf099770f441a60cfecc1702e3387a9f06197574
Vaengir <vaengir@gmx.de>
2023-06-19 16:05:45 +0200

    feat: Always run monitorsetup for work

--- startupLaptopWork/.local/bin/startup.sh
+++ startupLaptopWork/.local/bin/startup.sh
@@ -14,5 +14,7 @@ xset s noblank
 picom &
 # Allow Mousepad tapping to click
 xinput set-prop "$(xinput list --name-only | grep -i touch)" "libinput Tapping Enabled" 1
+# Monitor setup
+xrandr --output LVDS1 --mode 1920x1080 --pos 2560x700 --rotate normal --output DP1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP2 --off --output DP3 --off --output HDMI1 --off --output HDMI2 --off --output HDMI3 --off --output VGA1 --off --output VIRTUAL1 --off
 # Update all repos
 g pull ~/git/
