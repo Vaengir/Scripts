86e59d04eab05ba5504a4c82486141d96b724f98
Vaengir <vaengir@gmx.de>
2023-06-22 19:02:36 +0200

    fix: Adjust to new file setup

--- startupDesktop/.local/bin/startup.sh
+++ startupDesktop/.local/bin/startup.sh
@@ -13,6 +13,7 @@ xset s noblank
 # Run Compositor
 picom &
 # Update all repos
-g pull ~/git/
+g pull ~/personal/
+g pull ~/work/
 # Monitor setup
 xrandr --output DisplayPort-0 --off --output DisplayPort-1 --primary --mode 2560x1440 --rate 164.83 --pos 0x194 --rotate normal --output DisplayPort-2 --mode 1920x1080 --rate 164.92 --pos 2560x0 --rotate right --output HDMI-A-0 --off
