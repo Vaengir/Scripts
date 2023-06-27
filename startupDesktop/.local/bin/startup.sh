bb60e96a50b5ff4830c1579a04b27257ce7e568e
Vaengir <vaengir@gmx.de>
2023-06-27 22:09:19 +0200

    perf: Moving lines around
    Maybe the difference in order increases performance.

--- startupDesktop/.local/bin/startup.sh
+++ startupDesktop/.local/bin/startup.sh
@@ -10,10 +10,10 @@ xset b 0
 xset s off 
 xset -dpms 
 xset s noblank 
+# Monitor setup
+xrandr --output DisplayPort-0 --off --output DisplayPort-1 --primary --mode 2560x1440 --rate 164.83 --pos 0x194 --rotate normal --output DisplayPort-2 --mode 1920x1080 --rate 164.92 --pos 2560x0 --rotate right --output HDMI-A-0 --off
 # Run Compositor
 picom &
 # Update all repos
 g fetch ~/personal/
 g fetch ~/work/
-# Monitor setup
-xrandr --output DisplayPort-0 --off --output DisplayPort-1 --primary --mode 2560x1440 --rate 164.83 --pos 0x194 --rotate normal --output DisplayPort-2 --mode 1920x1080 --rate 164.92 --pos 2560x0 --rotate right --output HDMI-A-0 --off
