d2c33dc49ef6b69e568666b3ddbd8c22bf5bf4c0
Vaengir <vaengir@gmx.de>
2024-07-02 22:25:12 +0200

    fix: New Layout

--- startupDesktop/.local/bin/startup.sh
+++ startupDesktop/.local/bin/startup.sh
@@ -11,7 +11,7 @@ xset s off
 xset -dpms
 xset s noblank
 # Monitor setup
-xrandr --output DisplayPort-0 --off --output DisplayPort-1 --primary --mode 2560x1440 --rate 164.83 --pos 0x194 --rotate normal --output DisplayPort-2 --mode 1920x1080 --rate 164.92 --pos 2560x0 --rotate right --output HDMI-A-0 --off
+xrandr --output DisplayPort-0 --off --output DisplayPort-2 --primary --mode 2560x1440 --rate 164.83 --pos 0x167 --rotate normal --output DisplayPort-1 --mode 1920x1080 --rate 164.92 --pos 2560x0 --rotate right --output HDMI-A-0 --off
 # Run Compositor
 picom &
 # Update all repos
